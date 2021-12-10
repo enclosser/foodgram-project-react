d706becb14a0ef481406f323d6853cfd11f18277
andrey <66161872+Andrey-oss-ai@users.noreply.github.com>
2021-12-10 19:15:42 +0300

    flake8 refactoring v2

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -5,8 +5,11 @@ from . import views
 
 router_v1 = DefaultRouter()
 
-router_v1.register('ingredients', views.IngredientsViewSet,
-                basename='ingredients')
+router_v1.register(
+    'ingredients',
+    views.IngredientsViewSet,
+    basename='ingredients'
+)
 router_v1.register('tags', views.TagsViewSet, basename='tags')
 router_v1.register('recipes', views.RecipeViewSet, basename='recipes')
 
